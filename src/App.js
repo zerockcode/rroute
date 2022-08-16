aa6207d4954aef94b6329fbf218283d920f7e34b
kangckck25 <kangckck25@gmail.com>
2022-08-16 17:27:15 +0900

    Add Routers5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import {Route, Routes} from "react-router-dom";
+import TodoPage from "./pages/TodoPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/todo/*" element={<TodoPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoList(props) {
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+        </div>
+    );
+}
+
+export default TodoList;
